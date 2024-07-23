732e0fec9f472931e374a8686d5cf2f602d76dcf
claudioluciano <claudio_luciano@live.com>
2024-07-23 15:27:58 -0400

    fix multiples html build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                privacy: resolve(__dirname, 'privacy-policy.html'),
+            }
+        }
+    }
 })
